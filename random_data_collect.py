290c763bbd0d1243c041928ff20f4cd722d4459d
ChrisDeverall <40525895+ChrisDeverall@users.noreply.github.com>
2020-11-11 13:04:08 +0000

    Made the selection of subreddits non-random

--- random_data_collect.py
+++ random_data_collect.py
@@ -9,62 +9,64 @@ def get_random_posts(start_date, end_date, number_splits, list_of_subreddits, fi
     #pass dates as datetime objects
     
 
+
     time_intervals = np.round(np.linspace(int(time.mktime(start_date.timetuple())), int(time.mktime(end_date.timetuple())),number_splits)).astype(int)
     api = PushshiftAPI()
     
     for index in reversed(range(number_splits-1)):
-        print(index)
-        start_time = time_intervals[index]
-        end_time = time_intervals[index+1]
-        sub_reddit_rand = np.random.choice(list_of_subreddits)
-        print(sub_reddit_rand)
-        gen = api.search_submissions(subreddit = sub_reddit_rand,
-                         limit=100,
-                         before = end_time,
-                         after = start_time)
-        
-        new_selftext_list = []
-        new_title_list = []
-        new_id_list = []
-        new_date_list = []
-        
-        new_author = []
-        new_num_comments = []
-        # new_link = []
-        new_subreddit = []
+        for the_subreddit in list_of_subreddits:
+            print(index)
+            start_time = time_intervals[index]
+            end_time = time_intervals[index+1]
+            print(the_subreddit)
+            gen = api.search_submissions(subreddit = the_subreddit,
+                            limit=100,
+                            before = end_time,
+                            after = start_time)
+            
+            new_selftext_list = []
+            new_title_list = []
+            new_id_list = []
+            new_date_list = []
+            
+            new_author = []
+            new_num_comments = []
+            # new_link = []
+            new_subreddit = []
 
-        for post in gen:
-            try:
-                this_selftext= post.selftext
-                this_title = post.title
-                this_id = post.id
-                this_date = post.created_utc
-                this_author = post.author
-                this_num_comments = post.num_comments
-                # this_link = post.link
-                this_subreddit = post.subreddit
-                
-                new_selftext_list.append(this_selftext)
-                new_title_list.append(this_title)
-                new_id_list.append(this_id)
-                new_date_list.append(this_date)
-                new_author.append(this_author)
-                new_num_comments.append(this_num_comments)
-                # new_link.append(this_link)
-                new_subreddit.append(this_subreddit)
+            for post in gen:
+                try:
+                    this_selftext= post.selftext
+                    this_title = post.title
+                    this_id = post.id
+                    this_date = post.created_utc
+                    this_author = post.author
+                    this_num_comments = post.num_comments
+                    # this_link = post.link
+                    this_subreddit = post.subreddit
+                    
+                    new_selftext_list.append(this_selftext)
+                    new_title_list.append(this_title)
+                    new_id_list.append(this_id)
+                    new_date_list.append(this_date)
+                    new_author.append(this_author)
+                    new_num_comments.append(this_num_comments)
+                    # new_link.append(this_link)
+                    new_subreddit.append(this_subreddit)
 
-                
-            except Exception as e:
-                print(e)
-        
-        
-        append_df = pd.DataFrame([new_selftext_list, new_title_list, new_id_list, new_date_list, new_author, new_num_comments, new_subreddit]).T
-        
-        
-        with open(filename, 'a') as f:
-            append_df.to_csv(f, header=False)
+                    
+                except Exception as e:
+                    print(e)
+            
+            
+            append_df = pd.DataFrame([new_selftext_list, new_title_list, new_id_list, new_date_list, new_author, new_num_comments, new_subreddit]).T
+            
+            
+            with open(filename, 'a') as f:
+                append_df.to_csv(f, header=False)
 
-        print(this_date)
+            print(this_date)
+            print(len(new_selftext_list))
     return
 
 

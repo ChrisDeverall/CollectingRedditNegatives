719eaba7c19b269412f133c2d4c75ec4ec88f47b
ChrisDeverall <40525895+ChrisDeverall@users.noreply.github.com>
2020-11-11 02:46:33 +0000

    Updated for specified subreddits

--- random_data_collect.py
+++ random_data_collect.py
@@ -5,18 +5,18 @@ import datetime
 import time
 import sys
 
-def get_random_posts(start_date, end_date, number_splits, subreddit_df, filename):
+def get_random_posts(start_date, end_date, number_splits, list_of_subreddits, filename):
     #pass dates as datetime objects
     
 
     time_intervals = np.round(np.linspace(int(time.mktime(start_date.timetuple())), int(time.mktime(end_date.timetuple())),number_splits)).astype(int)
     api = PushshiftAPI()
     
-    for index in range(number_splits-1):
+    for index in reversed(range(number_splits-1)):
         print(index)
         start_time = time_intervals[index]
         end_time = time_intervals[index+1]
-        sub_reddit_rand = np.random.choice(subreddit_df.index, p=subreddit_df["chance"])
+        sub_reddit_rand = np.random.choice(list_of_subreddits)
         print(sub_reddit_rand)
         gen = api.search_submissions(subreddit = sub_reddit_rand,
                          limit=100,
@@ -27,30 +27,44 @@ def get_random_posts(start_date, end_date, number_splits, subreddit_df, filename
         new_title_list = []
         new_id_list = []
         new_date_list = []
+        
+        new_author = []
+        new_num_comments = []
+        # new_link = []
+        new_subreddit = []
+
         for post in gen:
             try:
                 this_selftext= post.selftext
                 this_title = post.title
                 this_id = post.id
                 this_date = post.created_utc
-
+                this_author = post.author
+                this_num_comments = post.num_comments
+                # this_link = post.link
+                this_subreddit = post.subreddit
                 
                 new_selftext_list.append(this_selftext)
                 new_title_list.append(this_title)
                 new_id_list.append(this_id)
                 new_date_list.append(this_date)
+                new_author.append(this_author)
+                new_num_comments.append(this_num_comments)
+                # new_link.append(this_link)
+                new_subreddit.append(this_subreddit)
+
                 
-            except:
-                print("Error")
+            except Exception as e:
+                print(e)
         
         
-        append_df = pd.DataFrame([new_selftext_list, new_title_list, new_id_list, new_date_list]).T
+        append_df = pd.DataFrame([new_selftext_list, new_title_list, new_id_list, new_date_list, new_author, new_num_comments, new_subreddit]).T
         
         
         with open(filename, 'a') as f:
             append_df.to_csv(f, header=False)
 
-        print(len(new_selftext_list))
+        print(this_date)
     return
 
 
@@ -67,9 +81,27 @@ n_splits = int(sys.argv[7])
 
 filename = sys.argv[8]
 
-subreddit_df = pd.read_csv("ranked_subreddits.csv")[["chance","subreddit_name"]].set_index("subreddit_name")
 
-get_random_posts(datetime.date(start_year,start_month,start_day), datetime.date(end_year,end_month,end_day), n_splits, subreddit_df, filename)
+#COPY AND PASTE YOUR LIST OF SUBREDDITS HERE AS A LIST OF STRINGS.
+list_of_subreddits = ["personalfinance",
+"TwoXChromosomes",
+"relationships",
+"buildapc",
+"truegaming",
+"TheRedPill",
+"worldbuilding",
+"dating_advice",
+"DecidingToBeBetter",
+"patientgamers",
+"productivity",
+"gonewildaudio",
+"Psychonaut",
+"TrueFilm",
+"TalesFromYourServer",
+"OverwatchUniversity"]
 
 
-#To get 8 sets of 100 posts from Jan 1 2020 to Jan 30 2020 and append data to file "my_negatives.csv", type: python3 random_data_collect.py 2020 1 1 2020 1 30 8 my_negatives.csv
+get_random_posts(datetime.date(start_year,start_month,start_day), datetime.date(end_year,end_month,end_day), n_splits, list_of_subreddits, filename)
+
+
+#To get 5000 sets of 100 posts from Jan 1 2015 to Nov 9 2020 and append data to file "my_negatives.csv", type: python3 random_data_collect.py 2015 1 1 2020 11 9 5000 my_negatives.csv
